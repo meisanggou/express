ae657773aedeab179c9e2340905b8c43ad5cc7df
meisanggou <zhou5315938@163.com>
2015-11-30 11:58:29 +0800

    change name

--- start_ex.sh
+++ start_ex.sh
@@ -1,11 +1,11 @@
 cd Web
-nohup python ex_web.py 1>> ex_web.log 2>>ex_web.log &
+nohup python ex_web.py 1>> log_web.log 2>>log_web.log &
 echo $! >> ../service.pid
 
 cd ../API_V1
-nohup python run.py 1>> api.log 2>>api.log &
+nohup python ex_api.py 1>> log_api.log 2>>log_api.log &
 echo $! >> ../service.pid
 
 cd ../Service
-nohup python QueryService.py 1>> query_service.log 2>> query_service.log &
+nohup python ex_QueryService.py 1>> log_query.log 2>> log_query.log &
 echo $! >> ../service.pid
