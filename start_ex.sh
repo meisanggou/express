9f1223ca1c858900330f967febdd985159cc3a95
meisanggou <zhou5315938@163.com>
2016-07-08 15:25:42 +0800

    删除API V1

--- start_ex.sh
+++ start_ex.sh
@@ -2,10 +2,6 @@ cd Web
 nohup python -u ex_web.py 1>> log_web.log 2>>log_web.log &
 echo $! >> ../service.pid
 
-cd ../API_V1
-nohup python -u ex_api.py 1>> log_api.log 2>>log_api.log &
-echo $! >> ../service.pid
-
 cd ../Service
 nohup python -u ex_QueryService.py 1>> log_query.log 2>> log_query.log &
 echo $! >> ../service.pid
