8c898feff5397624a47e11875ab2a7115f4dcb76
meisanggou <zhou5315938@163.com>
2016-08-29 16:03:50 +0800

    fix bug

--- Service/Express_Basic.py
+++ Service/Express_Basic.py
@@ -15,13 +15,13 @@ class ExpressBasic:
     def check_waybill(self, com, waybill):
         if len(waybill) > 20 or len(waybill) < 10:
             return False, u"运单号不正确", []
+        no = uuid.uuid1().hex
         search_result = re.search('[^0-9]', waybill)
         if search_result is not None:
             return False, u"运单号不正确", []
         result = self.eq.query(com, waybill)
         if result["status_code"] == 0:
-            return True, u"未查到相关快递信息", []
+            return True, no, []
         if result["completed"] is True:
             return False, u"运单已签收", []
-        no = uuid.uuid1().hex
         return True, no, result["express_info"]
