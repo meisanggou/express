c22789a0c1e513f31277f0a10135c7b3d518e62c
meisanggou <zhou5315938@163.com>
2017-06-13 08:43:13 +0800

    fix

--- Service/__init__.py
+++ Service/__init__.py
@@ -28,25 +28,25 @@ def after_request_func(resp):
 
 
 def handle_500_func(e):
-    level = "error"
-    info = request.data[:400] + "\n" + str(e)
-    info = info[:500].replace("\\", "\\\\").replace("\'", "\\'")
-    full_path = request.full_path[:200]
-    run_end = time()
-    if "run_begin" not in g:
-        run_begin = run_end
-    else:
-        run_begin = g.run_begin
-    run_time = run_end - run_begin
-    insert_sql = "INSERT INTO run_log(run_begin,host,url,method,account,ip,level,info,run_time) " \
-                 "VALUES (%s,'%s','%s','%s','%s',%s,'%s','%s',%s);" \
-                 % (run_begin, request.host_url, full_path, request.method, "", g.request_IP, level, info, run_time)
-    try:
-        db = DB()
-        db.execute(insert_sql)
-        db.close()
-    except Exception as e:
-        print(e)
+    # level = "error"
+    # info = request.data[:400] + "\n" + str(e)
+    # info = info[:500].replace("\\", "\\\\").replace("\'", "\\'")
+    # full_path = request.full_path[:200]
+    # run_end = time()
+    # if "run_begin" not in g:
+    #     run_begin = run_end
+    # else:
+    #     run_begin = g.run_begin
+    # run_time = run_end - run_begin
+    # insert_sql = "INSERT INTO run_log(run_begin,host,url,method,account,ip,level,info,run_time) " \
+    #              "VALUES (%s,'%s','%s','%s','%s',%s,'%s','%s',%s);" \
+    #              % (run_begin, request.host_url, full_path, request.method, "", g.request_IP, level, info, run_time)
+    # try:
+    #     db = DB()
+    #     db.execute(insert_sql)
+    #     db.close()
+    # except Exception as e:
+    #     print(e)
     resp = jsonify({"status": 2009901, "message": "Internal Error"})
     add_cross_domain_headers(resp)
     return resp
@@ -58,7 +58,7 @@ def create_app():
     app.before_request_funcs.setdefault(None, []).append(before_request_func)
     app.after_request_funcs.setdefault(None, []).append(after_request_func)
 
-    app.error_handler_spec[None][500] = handle_500_func
+    app.register_error_handler(500, handle_500_func)
 
     @app.route("/ping/", methods=["GET"])
     def ping():
